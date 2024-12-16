601133d5c1fc6aa8e3f6b76ca49ab01ad22ca677
AlwaysOnCue47 <martin.fobert@gmail.com>
2024-12-16 16:23:19 -0500

    Add project files.

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// tutorial assignment
+function newColor() {
+    document.getElementById("hello").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello").style.color = "darkolivegreen";
+}
+
+//animation
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
