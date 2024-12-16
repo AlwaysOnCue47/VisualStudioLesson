a4e060d8f7e48e9bc7f383931f38d5b60a5ae7df
AlwaysOnCue47 <martin.fobert@gmail.com>
2024-12-16 17:30:02 -0500

    Visual Studio final assignment

--- WebAppTutorial/wwwroot/js/site.js
+++ WebAppTutorial/wwwroot/js/site.js
@@ -13,20 +13,38 @@ function oldColor() {
 }
 
 //animation
-
-function myMove() {
-    let id = null;
+let id = null; // moved this variable outside of function for global scope
+let pos = 0; // moved this variable outside of function for global scope too - when animation restarts after stop it will start from it's last position
+function myMove() { // added more functionality to the W3School example. Animate continues until stop button is clicked. 
+    
     const elem = document.getElementById("animate");
-    let pos = 0;
+    
+    let direction = 1; //This variable is used to track the direction of the element
+
     clearInterval(id);
     id = setInterval(frame, 5);
+
     function frame() {
         if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
+            direction = -1; // Change direction to backward
+        } else if (pos == 0) {
+            direction = 1; // Change direction to forward
         }
+
+        pos += direction; // Increment or decrement position based on direction
+        elem.style.top = pos + "px";
+        elem.style.left = pos + "px";
     }
+    
 }
+
+document.getElementById("stopBtn").addEventListener("click", () => { // moved id outside of function so this event listener can reference it
+    if (id !== null) { // check if not null before resetting to avoid possible errors
+        clearInterval(id);
+        id = null; // Reset id to prevent re-clearing
+    }
+});
+
+
+
+
